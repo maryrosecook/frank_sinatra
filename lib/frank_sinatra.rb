0b2347301b5967bb1a90a747914a87ce1da66cb1
Mary Rose Cook <mary@maryrosecook.com>
2016-03-14 18:19:51 +0000

    make make file instantiate global app and proxy through API methods

---
+++ lib/frank_sinatra.rb
@@ -0,0 +1,13 @@
+require_relative './frank_sinatra/app'
+
+# start a global app
+@app = FrankSinatra::App.new
+
+# proxy through top-level API calls to global app
+def self.method_missing(method, *args, &block)
+  if [:get].include?(method)
+    @app.send(method, *args, &block)
+  else
+    super
+  end
+end
