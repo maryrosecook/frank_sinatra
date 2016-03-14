dff2ce0e985f258d35acbd8f2ace11d472eecc13
Mary Rose Cook <mary@maryrosecook.com>
2016-03-14 18:35:13 +0000

    set up the skeleton of a route recording system

--- lib/frank_sinatra/app.rb
+++ lib/frank_sinatra/app.rb
@@ -1,7 +1,21 @@
 module FrankSinatra
   class App
+    def initialize()
+      @routes = []
+    end
+
     def get(url, &block)
-      "got!"
+      @routes << Route.new(url, :get, block)
+    end
+  end
+
+  class Route
+    attr_accessor :url, :method, :block
+
+    def initialize(url, method, block)
+      @url = url
+      @method = method
+      @block = block
     end
   end
 end
